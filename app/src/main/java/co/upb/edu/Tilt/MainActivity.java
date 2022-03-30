4de202da0147ac4db4cbb6f432ffe787ccaa3e83
Candre <carlos.uribecupb.edu.co>
2022-03-30 15:04:16 -0500

    Cambios Hechos #2

--- app/src/main/java/co/upb/edu/Tilt/MainActivity.java
+++ app/src/main/java/co/upb/edu/Tilt/MainActivity.java
@@ -2,7 +2,10 @@ package co.upb.edu.Tilt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Botones
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        Button btnAcerca = (Button) findViewById(R.id.btnAcerca);
+        Button btnCreditos = (Button) findViewById(R.id.btnCreditos);
+
+        inicializarBotones(btnLogin, btnAcerca, btnCreditos);
     }
+
+    private void inicializarBotones (Button login, Button acerca, Button creditos){
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
