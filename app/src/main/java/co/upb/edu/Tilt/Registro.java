a7555d2c63511366455658a56eb7e4d372053c1b
Candre <carlos.uribecupb.edu.co>
2022-04-01 17:16:47 -0500

    Siguiendo con el firebase

--- app/src/main/java/co/upb/edu/Tilt/Registro.java
+++ app/src/main/java/co/upb/edu/Tilt/Registro.java
@@ -7,12 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Registro extends AppCompatActivity {
+
+    //Referencia a la BD
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        //Firebase
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //Botones
         Button btnRegistroVolver = (Button) findViewById(R.id.btnRegistroVolver);
