4bfd8a3a1eb86f219737c2f56208e67d459f37c0
Candre <carlos.uribecupb.edu.co>
2022-04-11 20:43:34 -0500

    Perfil Secundario Implementado

--- app/src/main/java/co/upb/edu/Tilt/ListaSeguidos.java
+++ app/src/main/java/co/upb/edu/Tilt/ListaSeguidos.java
@@ -1,5 +1,6 @@
 package co.upb.edu.Tilt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,7 +12,15 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ListaSeguidos extends AppCompatActivity {
+    private FirebaseDatabase usuariosBD;
+    private DatabaseReference losUsuariosBD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +31,13 @@ public class ListaSeguidos extends AppCompatActivity {
         Intent thisIntent = getIntent();
         String usuario = thisIntent.getStringExtra(MainActivity.usuarioActual);
 
+        //Firebase
+        usuariosBD = FirebaseDatabase.getInstance();
+        losUsuariosBD = usuariosBD.getReference("Usuarios");
+
         //Botones
-        ImageButton btnListaSeguidosHome = (ImageButton) findViewById(R.id.btn_listaSeguidos_home);
-        ImageButton btnListaSeguidosPerfil = (ImageButton) findViewById(R.id.btn_listaSeguidos_perfil);
+        ImageButton btnListaSeguidosHome = (ImageButton) findViewById(R.id.imgButton_ListaSeguidos_home);
+        ImageButton btnListaSeguidosPerfil = (ImageButton) findViewById(R.id.imgButton_ListaSeguidos_perfil);
         ImageButton btnBuscarPerfil = (ImageButton) findViewById(R.id.btnBuscarPerfil);
         inicializarBotones(btnListaSeguidosHome, btnListaSeguidosPerfil, btnBuscarPerfil, usuario);
     }
@@ -51,11 +64,30 @@ public class ListaSeguidos extends AppCompatActivity {
             public void onClick(View view) {
                 EditText usuarioBuscado = (EditText) findViewById(R.id.txtBuscarUsuarioPerfil);
                 String sUsuarioBuscado = usuarioBuscado.getText().toString();
+                final boolean[] buscoUnaVez = {false};
                 if(!TextUtils.isEmpty(sUsuarioBuscado)){
-                    Intent intent = new Intent(ListaSeguidos.this, perfilSecundario.class);
-                    intent.putExtra(MainActivity.usuarioActual, usuario);
-                    intent.putExtra(MainActivity.usuarioSeleccionado, sUsuarioBuscado);
-                    startActivity(intent);
+                    losUsuariosBD.child(sUsuarioBuscado).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.exists()){
+                                Intent intent = new Intent(ListaSeguidos.this, perfilSecundario.class);
+                                intent.putExtra(MainActivity.usuarioActual, usuario);
+                                intent.putExtra(MainActivity.usuarioSeleccionado, sUsuarioBuscado);
+                                startActivity(intent);
+                                buscoUnaVez[0] = true;
+                            }
+                            else if(!buscoUnaVez[0]){
+                                Toast.makeText(ListaSeguidos.this, "Este usuario no existe", Toast.LENGTH_LONG).show();
+                                buscoUnaVez[0] = true;
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
                 }
                 else{
                     Toast.makeText(ListaSeguidos.this, "Complete el campo para buscar", Toast.LENGTH_LONG).show();
--- app/src/main/java/co/upb/edu/Tilt/perfilSecundario.java
+++ app/src/main/java/co/upb/edu/Tilt/perfilSecundario.java
@@ -1,14 +1,160 @@
 package co.upb.edu.Tilt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class perfilSecundario extends AppCompatActivity {
+
+    //Referencia a la BD
+    private FirebaseDatabase usuariosBD;
+    private DatabaseReference losUsuariosBD;
+
+    private List<String> listaSeguidos;
+    private List<String> listaSeguidores;
+    private int seguidores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_secundario);
+
+        //Firebase
+        usuariosBD = FirebaseDatabase.getInstance();
+        losUsuariosBD = usuariosBD.getReference("Usuarios");
+
+        //Recibe usuarios
+        Intent thisIntent = getIntent();
+        String usuario = thisIntent.getStringExtra(MainActivity.usuarioActual);
+        String usuarioBuscado = thisIntent.getStringExtra(MainActivity.usuarioSeleccionado);
+
+        //Botones
+        ImageButton btnPerfilSecundarioHome = (ImageButton) findViewById(R.id.imgButton_perfilSecundario_home);
+        ImageButton btnPerfilSecundarioPerfil = (ImageButton) findViewById(R.id.imgButton_perfilSecundario_perfil);
+        Button btnSeguirNoseguir = (Button) findViewById(R.id.btn_perfilSecundario_seguir_dejar);
+
+        //Carga del perfil
+        cargandoPerfil(usuarioBuscado, usuario, btnSeguirNoseguir);
+
+        inicializarBotones(btnPerfilSecundarioHome, btnPerfilSecundarioPerfil,btnSeguirNoseguir, usuario, usuarioBuscado);
+    }
+
+    private void cargandoPerfil(String usuarioSecundario, String usuario, Button seguirNoseguir){
+        final boolean[] buscoUnaVez = {false};
+        final boolean[] buscoUnaVez2 = {false};
+
+        TextView nombrePerfil = (TextView) findViewById(R.id.text_perfilSecundario_NombreUsuario);
+        TextView infoPerfil = (TextView) findViewById(R.id.text_perfilSecundario_infoPerfil);
+        TextView numeroSeguidores = (TextView) findViewById(R.id.text_perfilSecundario_numeroSeguidores);
+
+        nombrePerfil.setText(usuarioSecundario);
+        losUsuariosBD.child(usuarioSecundario).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!buscoUnaVez[0]){
+                    infoPerfil.setText(dataSnapshot.child("correo").getValue().toString());
+                    if(dataSnapshot.child("listaSeguidores").exists()){
+                        listaSeguidores = (List<String>) dataSnapshot.child("listaSeguidores").getValue();
+                        seguidores = listaSeguidores.size();
+
+                    }
+                    else{
+                        listaSeguidores = new ArrayList<String>();
+                        seguidores = 0;
+                    }
+                    numeroSeguidores.setText(String.valueOf(seguidores));
+                }
+                buscoUnaVez[0] = true;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        losUsuariosBD.child(usuario).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!buscoUnaVez2[0]){
+                    if(!dataSnapshot.child("listaSeguidos").exists()){
+                        listaSeguidos = new ArrayList<String>();
+                        seguirNoseguir.setText("Seguir");
+                    }
+                    else{
+                        listaSeguidos = (List<String>) dataSnapshot.child("listaSeguidos").getValue();
+                        if(listaSeguidos.contains(usuarioSecundario)){
+                            seguirNoseguir.setText("Dejar de seguir");
+                        }
+                        else{
+                            seguirNoseguir.setText("Seguir");
+                        }
+                    }
+                }
+                buscoUnaVez2[0] = true;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void inicializarBotones(ImageButton home, ImageButton perfil, Button seguirNoseguir, String usuario, String usuarioBuscado){
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(perfilSecundario.this, Home.class);
+                intent.putExtra(MainActivity.usuarioActual, usuario);
+                startActivity(intent);
+            }
+        });
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(perfilSecundario.this, perfil.class);
+                intent.putExtra(MainActivity.usuarioActual, usuario);
+                startActivity(intent);
+            }
+        });
+
+        seguirNoseguir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(seguirNoseguir.getText().toString().equals("Seguir")){
+                    listaSeguidores.add(usuario);
+                    listaSeguidos.add(usuarioBuscado);
+                    seguidores++;
+                    seguirNoseguir.setText("Dejar de seguir");
+                }
+                else{
+                    listaSeguidores.remove(usuario);
+                    listaSeguidos.remove(usuarioBuscado);
+                    seguidores--;
+                    seguirNoseguir.setText("Dejar de seguir");
+                }
+                losUsuariosBD.child(usuarioBuscado).child("listaSeguidores").setValue(listaSeguidores);
+                losUsuariosBD.child(usuario).child("listaSeguidos").setValue(listaSeguidos);
+            }
+        });
     }
 }
